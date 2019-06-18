8fe13ca50fb216f09308c33bcb492adc4538adb9
radu-chira <radu.chira@plentydevelopment.com>
2019-06-18 13:39:27 +0300

    small change

--- src/Models/ToDo.php
+++ src/Models/ToDo.php
@@ -12,7 +12,7 @@ use Plenty\Modules\Plugin\DataBase\Contracts\Model;
  * @property int     $userId
  * @property boolean $isDone
  * @property int     $createdAt
- * @property string    $dateAt
+ * @property date    $dateAt
  */
 class ToDo extends Model
 {
