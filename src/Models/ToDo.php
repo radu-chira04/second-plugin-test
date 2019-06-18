2d36db58a8e23fbc0360fcc9a9aeb42c39f072f3
radu-chira <radu.chira@plentydevelopment.com>
2019-06-18 10:04:25 +0300

    add the code

--- src/Models/ToDo.php
+++ src/Models/ToDo.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace ToDoList\Models;
+
+use Plenty\Modules\Plugin\DataBase\Contracts\Model;
+
+/**
+ * Class ToDo
+ *
+ * @property int     $id
+ * @property string  $taskDescription
+ * @property int     $userId
+ * @property boolean $isDone
+ * @property int     $createdAt
+ */
+class ToDo extends Model
+{
+    /**
+     * @var int
+     */
+    public $id              = 0;
+    public $taskDescription = '';
+    public $userId          = 0;
+    public $isDone          = false;
+    public $createdAt       = 0;
+
+    /**
+     * @return string
+     */
+    public function getTableName(): string
+    {
+        return 'ToDoList::ToDo';
+    }
+}
